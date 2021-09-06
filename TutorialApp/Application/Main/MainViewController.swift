02126cc1a1672a6f05119ac20b4da226b66bee75
Alejandro Taborda <alejandro.taborda.60@gmail.com>
2021-09-06 11:50:25 -0300

    First commit of xib Folder

---
+++ TutorialApp/Application/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  TutorialApp
+//
+//  Created by Alejandro Exequiel Taborda on 03/01/2021.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
